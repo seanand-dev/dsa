a38c72886f3c2a8ec2309fb39b22582e2c315622
seanand-dev <85431731+seanand-dev@users.noreply.github.com>
2021-09-18 15:41:16 +0530

    array ques

--- dsaProblems/src/dsaProblems/arrayds/ArrayDS.java
+++ dsaProblems/src/dsaProblems/arrayds/ArrayDS.java
@@ -320,6 +320,43 @@ public class ArrayDS {
 	        }
 	        return result;
 	        }
+	    
+	    public int[] productExceptSelf(int[] nums) {//[1,2,3,4]
+	        int n = nums.length;
+	        int[] res = new int[n];
+	        // Calculate lefts and store in res.
+	        int left = 1;
+	        for (int i = 0; i < n; i++) {
+	            if (i > 0)
+	                left = left * nums[i - 1];
+	            res[i] = left;
+	        }
+	        // Calculate rights and the product from the end of the array.
+	        int right = 1;
+	        for (int i = n - 1; i >= 0; i--) {
+	            if (i < n - 1)
+	                right = right * nums[i + 1];
+	            res[i] *= right;
+	        }
+	        return res;
+	    }
+	    
+	    public int findDuplicate(int[] nums) {
+	        int slow=nums[0];
+	        int fast=nums[nums[0]];
+	        while(slow!=fast){
+	            slow=nums[slow];
+	            fast=nums[nums[fast]];
+	        }
+	        fast=0;
+	          while(slow!=fast){
+	            slow=nums[slow];
+	            fast=nums[fast];
+	        }
+	        return slow;
+	        
+	    }
+	
 	        
 	    
 }
